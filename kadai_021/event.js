ee3a4ee1eeb41d6979e9d243d5f85c0509da3108
asaharap <asahara.kat@gmail.com>
2025-01-25 00:03:15 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+    console.log('ボタンをクリックしました');
+    text.textContent = 'ボタンをクリックしました';
+  }, 2000);
+  
+});
